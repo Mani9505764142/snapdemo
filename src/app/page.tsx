c7338a64bdd5b5b0d68fd677856eb58f78ccf728
Suthari Saimanikanta Vivek <manikanta.suthari2002@gmail.com>
2026-01-12 16:05:49 +0530

    Initial SnapDemo setup with Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <a
+        href="/record"
+        className="px-6 py-3 bg-black text-white rounded-xl"
+      >
+        Start Recording
+      </a>
+    </main>
+  );
+}
